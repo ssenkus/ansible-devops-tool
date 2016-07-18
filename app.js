29e33d6a7bb1e188470ce76117ead07b98957e16
Senkus, Steven <steven.senkus@dat.com>
2016-07-17 17:30:36 -0700

    basic saving of command output

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ var spawn = child_process.spawn,
     server = http.Server(app),
     io = socketIO(server);
 
+
+var MongoClient = require('mongodb').MongoClient;
+
+
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
@@ -21,7 +25,6 @@ app.use(express.static('public'));
 app.get('/', auth, function (req, res) {
     fs.createReadStream('./public/index.html').pipe(res)
 });
-
 
 app.get('/inventory', auth, function (req, res) {
     var inventoryFile = req.query.inventoryFile;
@@ -35,6 +38,9 @@ app.get('/inventory', auth, function (req, res) {
         });
     }
 });
+
+app.post('command');
+
 
 
 server.listen(3000, function () {
@@ -69,6 +75,7 @@ io.on('connection', function (socket) {
 });
 
 
+
 function auth(req, res, next) {
 
     function unauthorized(res) {
@@ -94,7 +101,11 @@ function auth(req, res, next) {
 function runCommands(socket, commands, callback) {
     var command = commands.shift().split(' ');
     var process = spawn(command[0], command.slice(1));
+
+    var fullData = '';
+
     var outputHandler = function (data) {
+        fullData += data.toString();
         socket.emit('out', {
             command: command,
             output: data.toString(),
@@ -105,11 +116,40 @@ function runCommands(socket, commands, callback) {
     process.stdout.on('data', outputHandler);
     process.stderr.on('data', outputHandler);
 
+
     process.on('close', function (code) {
         if (code === 0 && commands.length > 0) {
             runCommands(socket, commands, callback);
         } else {
+            saveCommandOutput(fullData);
             callback();
         }
+    });
+}
+
+function saveCommandOutput(commandOutput) {
+    console.log(commandOutput);
+    MongoClient.connect('mongodb://localhost:27017/devopsdb', function(err, db) {
+        if (err) {
+            throw err;
+        }
+        console.log('connected to mongo');
+
+        var collection = db.collection('command_output');
+
+        //Test command output
+        var command = {timestamp: Math.floor(new Date() / 1000), output: commandOutput};
+
+        // Insert some users
+        collection.insert(command, function (err, result) {
+            console.log(result);
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Successfully inserted command output to mongodb');
+            }
+
+            db.close();
+        });
     });
 }
